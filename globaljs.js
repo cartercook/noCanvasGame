fbb8ec9b5c1b855457809622210c0408edab3294
Jacob Chamberlain <jacob0584@gmail.com>
2017-06-14 13:41:12 -0400

    added more levels, and fixed some stuff

---
+++ globaljs.js
@@ -0,0 +1,59 @@
+function createPlayer(xPos, yPos, dom) {
+	Crafty.load({
+	    "sprites": {
+	        // This spritesheet has 8 images, in a 1 by 8 grid
+	        "player.png": {
+	            // This is the width of each image in pixels
+	            tile: 25,
+	            // The height of each image
+	            tileh: 25,
+	            // We give names to individual images
+	            map: {
+	            	player_stand: [0, 0]
+	            }
+	        }
+	    }
+	},
+	function() {
+		var mode;
+		if (dom) {
+			mode = 'DOM';
+		} else {
+			mode = 'Canvas';
+		}
+		
+		var player = Crafty.e('2D, Twoway, Gravity, player_stand, SpriteAnimation, ' + mode)
+			.attr({x: xPos, y: yPos, w: 25, h: 25})
+			.twoway(150)
+			.gravity('Floor')
+			.reel("player_stand", 1000, [[0, 0]])
+		    .reel("player_jump", 1000, [[1, 0]])
+			.reel("player_walk", 500, [
+				[2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]
+			])
+			.animate("player_walk", -1)
+			.bind('MotionChange', function(change) {
+				if(this.vx === 0) {
+					this.reel('player_stand');
+				} else {
+					this.animate('player_walk', -1);
+				}
+			});
+	});
+}
+
+function killfloor(ref){
+	alert("You died");
+    window.location.href = ref;
+}
+
+function gate(ref){
+	alert("okay to next level");
+    window.location.href = ref;
+}
+
+function wall(data){
+    //get object that hit (always player)
+    //find out where it hit from
+    // move ouside of block
+}
